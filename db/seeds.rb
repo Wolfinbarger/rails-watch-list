cc3e108af42df52310370280132df9e57d206a7e
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-05-12 15:59:15 +0900

    seed destroy order edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Movie.destroy_all
-List.destroy_all
 Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
 
 10.times do
   Movie.create!(title: Faker::Movie.title,
